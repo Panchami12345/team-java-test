a020cc0d20aa32ad5e60bfee0f0a4ffb9c09d252
Panchami Urs S <panchamiurs2006@gmail.com>
2026-03-15 00:49:21 +0530

    Implement array reversal in q2.java

---
+++ q2.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class q2 {
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int temp;
+
+        for(int i=0;i<n/2;i++){
+            temp = arr[i];
+            arr[i] = arr[n-i-1];
+            arr[n-i-1] = temp;
+        }
+
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
